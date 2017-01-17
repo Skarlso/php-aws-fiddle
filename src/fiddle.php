a6a44d3a77e7a46cc942a72b7c49606d11ea8eaa
Gergely Brautigam <skarlso777@gmail.com>
2017-01-17 09:56:16 +0100

    Initial commit.

---
+++ src/fiddle.php
@@ -0,0 +1,8 @@
+<?php
+// Include the SDK using the Composer autoloader
+require 'vendor/autoload.php';
+
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'eu-central-1'
+]);
