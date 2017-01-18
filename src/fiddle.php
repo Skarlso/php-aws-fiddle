465f08f3b8d7072659d5f5539b7da73eb9e3155f
Gergely Brautigam <skarlso777@gmail.com>
2017-01-18 08:25:28 +0100

    Namespacing and creating a binary.

--- src/fiddle.php
+++ src/fiddle.php
@@ -1,14 +1,22 @@
-#!/usr/local/opt/php70/bin/php
 <?php
-// Include the SDK using the Composer autoloader
+namespace Fiddle;
 require 'vendor/autoload.php';
 
 use Monolog\Logger;
 
-$log = new Logger('main');
-$log->info('Initiating AWS S3 client.');
+class Fiddle {
+    public $log;
 
-$s3 = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'  => 'eu-central-1'
-]);
+    public function __construct() {
+        $this->log = new Logger("main");
+    }
+
+    public function start() {
+        $this->log->info('Initiating AWS S3 client.');
+
+        $s3 = new \Aws\S3\S3Client([
+            'version' => 'latest',
+            'region'  => 'eu-central-1'
+        ]);
+    }
+}
